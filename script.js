180a45e3b6471ac5f5d5ee9d16e506822ca06725
Cate Dunn <60521577+CateDunn@users.noreply.github.com>
2020-03-30 14:17:15 -0400

    updates

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
-console.log('testing')
-
 var searchedCity = $('#searched-city').val().trim();
 var APIkey = '5cd539647450f9a07b96edfc16f158cb';
 var format = ('L');
 var moment = moment();
-//curent date (automatically updates)
 var dateResult = moment.format(format);
 var last = localStorage.getItem('city');
-var lastCity = localStorage.getItem('last city')
+var lastCity = localStorage.getItem('last city');
+var list = localStorage.getItem('search list');
+
 
 
 $(document).ready(function Render(){
+  //when the document loads, the function is run using the last searched city
 
-  var searchedCity = $('#searched-city').val().trim();
-  var city = document.createElement("tr");
-  //add city typed in to list below
-  city.textContent = searchedCity;
-  $('#search-history').prepend(city);
-  //save to local storage
-  localStorage.setItem('city', searchedCity); 
-
+  
   //API call 1
   queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + lastCity + '&appid=5cd539647450f9a07b96edfc16f158cb'
   $.ajax({
@@ -87,11 +80,11 @@ $(document).ready(function Render(){
   $('.p-2').addClass('boxes');
 
   //date
-  var date1 = response.list[5].dt_txt;
-  var date2 = response.list[13].dt_txt;
-  var date3 = response.list[21].dt_txt;
-  var date4 = response.list[29].dt_txt;
-  var date5 = response.list[37].dt_txt;
+  var date1 = response.list[6].dt_txt;
+  var date2 = response.list[14].dt_txt;
+  var date3 = response.list[22].dt_txt;
+  var date4 = response.list[30].dt_txt;
+  var date5 = response.list[38].dt_txt;
   $('.date1').html(date1);
   $('.date2').html(date2);
   $('.date3').html(date3);
@@ -99,28 +92,28 @@ $(document).ready(function Render(){
   $('.date5').html(date5);
 
   //icon 
-  var icon1 = response.list[5].weather[0].icon;
+  var icon1 = response.list[6].weather[0].icon;
   var iconUrl1 = "http://openweathermap.org/img/w/" + icon1 + ".png";
   $('.icon1').html("<img src='" + iconUrl1  + "'>");
-  var icon2 = response.list[13].weather[0].icon;
+  var icon2 = response.list[14].weather[0].icon;
   var iconUrl2 = "http://openweathermap.org/img/w/" + icon2 + ".png";
   $('.icon2').html("<img src='" + iconUrl2  + "'>");
-  var icon3 = response.list[21].weather[0].icon;
+  var icon3 = response.list[22].weather[0].icon;
   var iconUrl3 = "http://openweathermap.org/img/w/" + icon3+ ".png";
   $('.icon3').html("<img src='" + iconUrl3  + "'>");
-  var icon4 = response.list[29].weather[0].icon;
+  var icon4 = response.list[30].weather[0].icon;
   var iconUrl4 = "http://openweathermap.org/img/w/" + icon4 + ".png";
   $('.icon4').html("<img src='" + iconUrl4  + "'>");
-  var icon5 = response.list[37].weather[0].icon;
+  var icon5 = response.list[38].weather[0].icon;
   var iconUrl5 = "http://openweathermap.org/img/w/" + icon5 + ".png";
   $('.icon5').html("<img src='" + iconUrl5  + "'>");
   
   //temp (converted to Farenheit)
-  var temp1 = (response.list[5].main.temp - 273.15) * 1.80 + 32;
-  var temp2 = (response.list[13].main.temp - 273.15) * 1.80 + 32;
-  var temp3 = (response.list[21].main.temp - 273.15) * 1.80 + 32;
-  var temp4 = (response.list[29].main.temp - 273.15) * 1.80 + 32;
-  var temp5 = (response.list[37].main.temp - 273.15) * 1.80 + 32;
+  var temp1 = (response.list[6].main.temp - 273.15) * 1.80 + 32;
+  var temp2 = (response.list[14].main.temp - 273.15) * 1.80 + 32;
+  var temp3 = (response.list[22].main.temp - 273.15) * 1.80 + 32;
+  var temp4 = (response.list[30].main.temp - 273.15) * 1.80 + 32;
+  var temp5 = (response.list[38].main.temp - 273.15) * 1.80 + 32;
   $('.temp1').text("Temp: " + temp1.toFixed(1) + " ° F");
   $('.temp2').text("Temp: " + temp2.toFixed(1) + " ° F");
   $('.temp3').text("Temp: " + temp3.toFixed(1) + " ° F");
@@ -128,21 +121,25 @@ $(document).ready(function Render(){
   $('.temp5').text("Temp: " + temp5.toFixed(1) + " ° F");
 
   //humidity
-  $('.humidity1').text('Humidity: ' + response.list[5].main.humidity + "%");
-  $('.humidity2').text('Humidity: ' + response.list[13].main.humidity + "%");
-  $('.humidity3').text('Humidity: ' + response.list[21].main.humidity + "%");
-  $('.humidity4').text('Humidity: ' + response.list[29].main.humidity + "%");
-  $('.humidity5').text('Humidity: ' + response.list[37].main.humidity + "%");
+  $('.humidity1').text('Humidity: ' + response.list[6].main.humidity + "%");
+  $('.humidity2').text('Humidity: ' + response.list[14].main.humidity + "%");
+  $('.humidity3').text('Humidity: ' + response.list[22].main.humidity + "%");
+  $('.humidity4').text('Humidity: ' + response.list[30].main.humidity + "%");
+  $('.humidity5').text('Humidity: ' + response.list[38].main.humidity + "%");
   
   }); 
 
-  
+  // repeating above function when search button is clicked using the city 
+  //that is typed in
   $('.btn').click(function(){
     var searchedCity = $('#searched-city').val().trim();
     var city = document.createElement("tr");
     //add city typed in to list below
     city.textContent = searchedCity;
     $('#search-history').prepend(city);
+    var searchHistory = document.getElementById('search-history')
+    console.log(searchHistory)
+    localStorage.setItem('search list', searchHistory);
     //save to local storage
     localStorage.setItem('city', searchedCity); 
 
@@ -214,11 +211,11 @@ $(document).ready(function Render(){
       $('.p-2').addClass('boxes');
 
       //date
-      var date1 = response.list[5].dt_txt;
-      var date2 = response.list[13].dt_txt;
-      var date3 = response.list[21].dt_txt;
-      var date4 = response.list[29].dt_txt;
-      var date5 = response.list[37].dt_txt;
+      var date1 = response.list[6].dt_txt;
+      var date2 = response.list[14].dt_txt;
+      var date3 = response.list[22].dt_txt;
+      var date4 = response.list[30].dt_txt;
+      var date5 = response.list[38].dt_txt;
       $('.date1').html(date1);
       $('.date2').html(date2);
       $('.date3').html(date3);
@@ -226,28 +223,28 @@ $(document).ready(function Render(){
       $('.date5').html(date5);
 
       //icon 
-      var icon1 = response.list[5].weather[0].icon;
+      var icon1 = response.list[6].weather[0].icon;
       var iconUrl1 = "http://openweathermap.org/img/w/" + icon1 + ".png";
       $('.icon1').html("<img src='" + iconUrl1  + "'>");
-      var icon2 = response.list[13].weather[0].icon;
+      var icon2 = response.list[14].weather[0].icon;
       var iconUrl2 = "http://openweathermap.org/img/w/" + icon2 + ".png";
       $('.icon2').html("<img src='" + iconUrl2  + "'>");
-      var icon3 = response.list[21].weather[0].icon;
+      var icon3 = response.list[22].weather[0].icon;
       var iconUrl3 = "http://openweathermap.org/img/w/" + icon3+ ".png";
       $('.icon3').html("<img src='" + iconUrl3  + "'>");
-      var icon4 = response.list[29].weather[0].icon;
+      var icon4 = response.list[30].weather[0].icon;
       var iconUrl4 = "http://openweathermap.org/img/w/" + icon4 + ".png";
       $('.icon4').html("<img src='" + iconUrl4  + "'>");
-      var icon5 = response.list[37].weather[0].icon;
+      var icon5 = response.list[38].weather[0].icon;
       var iconUrl5 = "http://openweathermap.org/img/w/" + icon5 + ".png";
       $('.icon5').html("<img src='" + iconUrl5  + "'>");
       
       //temp (converted to Farenheit)
-      var temp1 = (response.list[5].main.temp - 273.15) * 1.80 + 32;
-      var temp2 = (response.list[13].main.temp - 273.15) * 1.80 + 32;
-      var temp3 = (response.list[21].main.temp - 273.15) * 1.80 + 32;
-      var temp4 = (response.list[29].main.temp - 273.15) * 1.80 + 32;
-      var temp5 = (response.list[37].main.temp - 273.15) * 1.80 + 32;
+      var temp1 = (response.list[6].main.temp - 273.15) * 1.80 + 32;
+      var temp2 = (response.list[14].main.temp - 273.15) * 1.80 + 32;
+      var temp3 = (response.list[22].main.temp - 273.15) * 1.80 + 32;
+      var temp4 = (response.list[30].main.temp - 273.15) * 1.80 + 32;
+      var temp5 = (response.list[38].main.temp - 273.15) * 1.80 + 32;
       $('.temp1').text("Temp: " + temp1.toFixed(1) + " ° F");
       $('.temp2').text("Temp: " + temp2.toFixed(1) + " ° F");
       $('.temp3').text("Temp: " + temp3.toFixed(1) + " ° F");
@@ -255,20 +252,13 @@ $(document).ready(function Render(){
       $('.temp5').text("Temp: " + temp5.toFixed(1) + " ° F");
 
       //humidity
-      $('.humidity1').text('Humidity: ' + response.list[5].main.humidity + "%");
-      $('.humidity2').text('Humidity: ' + response.list[13].main.humidity + "%");
-      $('.humidity3').text('Humidity: ' + response.list[21].main.humidity + "%");
-      $('.humidity4').text('Humidity: ' + response.list[29].main.humidity + "%");
-      $('.humidity5').text('Humidity: ' + response.list[37].main.humidity + "%");
+      $('.humidity1').text('Humidity: ' + response.list[6].main.humidity + "%");
+      $('.humidity2').text('Humidity: ' + response.list[14].main.humidity + "%");
+      $('.humidity3').text('Humidity: ' + response.list[22].main.humidity + "%");
+      $('.humidity4').text('Humidity: ' + response.list[30].main.humidity + "%");
+      $('.humidity5').text('Humidity: ' + response.list[38].main.humidity + "%");
       
       });   
     });
-
-
-
-
-
-
-
 
 });
